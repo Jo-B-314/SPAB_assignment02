8c7cbf17613d01c63662286106aeb3a646733353
Lisa <63642930+CorvusCorax95@users.noreply.github.com>
2021-11-02 09:57:44 +0100

    setted all assignments

---
+++ Assignment2-1.py
@@ -0,0 +1,15 @@
+
+import math
+
+##
+# Which average coverage is required, such that the fraction of the genome seen 
+# less than t times, is much smaller than 1bp, say, q.
+##
+
+m = 3*pow(10,9)
+t = 10
+q = 1 /(100*m)
+
+print(m)
+print(t)
+print(q)
